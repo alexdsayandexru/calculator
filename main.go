a7692ae07764226a488559f3f0c7931a6c0b683d
aleksandrsudakov <alexdsa@yandex.ru>
2023-01-24 18:31:22 +0300

    add expand method

--- main.go
+++ main.go
@@ -6,17 +6,49 @@ import (
 	"strings"
 )
 
+func calculate(v1 int, o string, v2 int) int {
+	switch o {
+	case "+":
+		return v1 + v2
+	case "-":
+		return v1 - v2
+	case "*":
+		return v1 * v2
+	case "/":
+		return v1 / v2
+	}
+	panic("invalid operation:" + o)
+}
+
+func expand(exp []interface{}) []interface{} {
+	for i := 0; i < len(exp)-2; i++ {
+		o1, ok1 := exp[i].(string)
+		v, ok2 := exp[i+1].(int)
+		o2, ok3 := exp[i+2].(string)
+
+		if ok1 && ok2 && ok3 && o1 == "(" && o2 == ")" {
+			nexp := append(exp[:i+1])
+			nexp[i] = v
+			exp = append(nexp, exp[i+3:]...)
+			i = i - 1
+		}
+	}
+	return exp
+}
+
 func calc(exp []interface{}) []interface{} {
 	for i := 0; i < len(exp)-2; i++ {
 		v1, ok1 := exp[i].(int)
-		v2, ok2 := exp[i+1].(string)
-		v3, ok3 := exp[i+2].(int)
+		o, ok2 := exp[i+1].(string)
+		v2, ok3 := exp[i+2].(int)
 
 		if ok1 && ok2 && ok3 {
-			fmt.Println(i, v1, v2, v3)
+			//fmt.Println(v1, o, v2)
 			nexp := append(exp[:i+1])
-			nexp[i] = v1 - v3
+			nexp[i] = calculate(v1, o, v2)
 			exp = append(nexp, exp[i+3:]...)
+			i = i - 1
+			//fmt.Println(exp)
 		}
 	}
 	return exp
@@ -24,18 +56,17 @@ func calc(exp []interface{}) []interface{} {
 
 func main() {
 	tokens := []interface{}{}
-	expression := "10 + ( 40 - ( 12 - 3 ) - ( 5 - 2 ) ) * 10 / 5"
-	s1 := strings.Split(expression, " ")
-	//fmt.Println(s1)
-	//s2 := append(s1[:12], s1[15:]...)
-	//fmt.Println(s2)
-	for _, s := range s1 {
+	expression := "10 + ( 40 - ( 12 - 3 + 4 ) - ( 5 - 2 - 10 ) ) * 10 / 5"
+
+	for _, s := range strings.Split(expression, " ") {
 		if v, err := strconv.Atoi(s); err == nil {
-			//fmt.Println(v)
 			tokens = append(tokens, v)
 		} else {
 			tokens = append(tokens, s)
 		}
 	}
-	fmt.Println(calc(tokens))
+	tokens = calc(tokens)
+	fmt.Println(tokens)
+	tokens = expand(tokens)
+	fmt.Println(tokens)
 }
