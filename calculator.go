1418de15622b778209d05307b82020a401c4e482
Alex <alexdsa@yandex.ru>
2023-01-25 17:28:47 +0300

    Calculator versiov 1.0

--- calculator.go
+++ calculator.go
@@ -1,10 +1,115 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 type Calculator struct {
 }
 
-func (c Calculator) Calculate(expression string) {
-	fmt.Println("Hello from calculator:", expression)
+func (c Calculator) Calculate(expression string) (int, bool) {
+	tokens := splitExpression(expression)
+	tokens = calculateTokens(tokens)
+	value, ok := tokens[0].(int)
+	if ok {
+		return value, true
+	}
+	return 0, false
+}
+
+func printTokens(tokens []interface{}){
+	for i, token := range tokens {
+		value, ok := token.(int)
+		operator, _ := token.(string)
+
+		if(ok) {
+			fmt.Println(i, "v:", value)
+		} else {
+			fmt.Println(i, "o:", operator)
+		}
+	}
+}
+
+func calculate(left int, right int, operator string) int {
+	switch operator {
+	case "+":
+		return left + right
+	case "-":
+		return left - right
+	case "*":
+		return left * right
+	case "/":
+		return left / right
+	}
+	panic("invalid operator:" + operator)
+}
+
+func splitExpression (expression string) []interface{} {
+	tokens := []interface{}{}
+	for _, s := range strings.Split(expression, " ") {
+		if v, err := strconv.Atoi(s); err == nil {
+			tokens = append(tokens, v)
+		} else {
+			tokens = append(tokens, s)
+		}
+	}
+	return tokens
+}
+
+func isSimpleExpression(tokens []interface{}, i int) (bool, int, int, string) {
+	left, ok_left := tokens[i].(int)
+	operator, ok_operator := tokens[i+1].(string)
+	right, ok_right := tokens[i+2].(int)
+
+	if ok_left && ok_operator && ok_right {
+		return true, left, right, operator
+	}
+	return false, left, right, operator
+}
+
+func isBrackets(tokens []interface{}, i int) (bool, int) {
+	if len(tokens) == 3 {
+		i = 0
+	}
+	o1, ok1 := tokens[i].(string)
+	value, ok2 := tokens[i+1].(int)
+	o2, ok3 := tokens[i+2].(string)
+
+	if ok1 && ok2 && ok3 && o1 == "(" && o2 == ")" {
+		return true, value
+	}
+	return false, value
+}
+
+func calculateTokens(tokens []interface{}) []interface{} {
+	for i := 0; i < len(tokens)-2; i++ {
+		if len(tokens) >=3 {
+			ok, left, right, operator := isSimpleExpression(tokens, i)
+			if ok {
+				value := calculate(left, right, operator)
+				tokens = transformTokens(tokens, i, value)
+				i = i - 1
+			}
+		}
+		if len(tokens) >=3 {
+			ok, value := isBrackets(tokens, i)
+			if ok {
+				tokens = transformTokens(tokens, i, value)
+				i = 0
+			}
+		}
+		if len(tokens) == 3 {
+			tokens = calculateTokens(tokens)
+		}
+	}
+	return tokens
+}
+
+func transformTokens(tokens []interface{}, i int, value int)  []interface{} {
+	new_tokens := append(tokens[:i+1])
+	new_tokens[i] = value
+	tokens = append(new_tokens, tokens[i+3:]...)
+	return tokens
 }
--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
-func calculate(v1 int, o string, v2 int) int {
-	switch o {
-	case "+":
-		return v1 + v2
-	case "-":
-		return v1 - v2
-	case "*":
-		return v1 * v2
-	case "/":
-		return v1 / v2
-	}
-	panic("invalid operation:" + o)
-}
 
 func expand(exp []interface{}) []interface{} {
 	for i := 0; i < len(exp)-2; i++ {
@@ -36,37 +19,14 @@ func expand(exp []interface{}) []interface{} {
 	return exp
 }
 
-func calc(exp []interface{}) []interface{} {
-	for i := 0; i < len(exp)-2; i++ {
-		v1, ok1 := exp[i].(int)
-		o, ok2 := exp[i+1].(string)
-		v2, ok3 := exp[i+2].(int)
-
-		if ok1 && ok2 && ok3 {
-			//fmt.Println(v1, o, v2)
-			nexp := append(exp[:i+1])
-			nexp[i] = calculate(v1, o, v2)
-			exp = append(nexp, exp[i+3:]...)
-			i = i - 1
-			//fmt.Println(exp)
-		}
-	}
-	return exp
-}
 
 func main() {
-	tokens := []interface{}{}
-	expression := "10 + ( 40 - ( 12 - 3 + 4 ) - ( 5 - 2 - 10 ) ) * 10 / 5"
+	fmt.Println("")
+	
+	expression := "10 + ( 40 - ( 12 - ( 3 + 4 ) ) - ( 5 - 2 - 10 ) ) * ( 5 + ( 10 / 5 ) )"
+	//expression := "( 10 + 40 )"
+	//expression := "100"
 
-	for _, s := range strings.Split(expression, " ") {
-		if v, err := strconv.Atoi(s); err == nil {
-			tokens = append(tokens, v)
-		} else {
-			tokens = append(tokens, s)
-		}
-	}
-	tokens = calc(tokens)
-	fmt.Println(tokens)
-	tokens = expand(tokens)
-	fmt.Println(tokens)
+	calc := Calculator{}
+	fmt.Println(calc.Calculate(expression))
 }
